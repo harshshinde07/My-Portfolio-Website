598edab30f8462bcb60e05cc96b6a1f6862177ab
Harsh <harshshinde07@gmail.com>
2020-08-27 18:08:59 +0530

    scaffolding the structure of website

---
+++ android/app/src/main/kotlin/in/co/apps/harsh/portfolio_web/MainActivity.kt
@@ -0,0 +1,6 @@
+package in.co.apps.harsh.portfolio_web
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/widgets/drawer.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+
+class MobileDrawer extends StatefulWidget {
+  const MobileDrawer({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  _MobileDrawerState createState() => _MobileDrawerState();
+}
+
+class _MobileDrawerState extends State<MobileDrawer> {
+  bool _isProcessing = false;
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Container(
+        color: Theme.of(context).bottomAppBarColor,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              InkWell(
+                onTap: () {},
+                child: Text(
+                  'HOME',
+                  style: TextStyle(color: Colors.white, fontSize: 22),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 5.0, bottom: 5.0),
+                child: Divider(
+                  color: Colors.blueGrey[400],
+                  thickness: 2,
+                ),
+              ),
+              InkWell(
+                onTap: () {},
+                child: Text(
+                  'ABOUT',
+                  style: TextStyle(color: Colors.white, fontSize: 22),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 5.0, bottom: 5.0),
+                child: Divider(
+                  color: Colors.blueGrey[400],
+                  thickness: 2,
+                ),
+              ),
+              InkWell(
+                onTap: () {},
+                child: Text(
+                  'RESUME',
+                  style: TextStyle(color: Colors.white, fontSize: 22),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 5.0, bottom: 5.0),
+                child: Divider(
+                  color: Colors.blueGrey[400],
+                  thickness: 2,
+                ),
+              ),
+              InkWell(
+                onTap: () {},
+                child: Text(
+                  'PROJECTS',
+                  style: TextStyle(color: Colors.white, fontSize: 22),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 5.0, bottom: 5.0),
+                child: Divider(
+                  color: Colors.blueGrey[400],
+                  thickness: 2,
+                ),
+              ),
+              InkWell(
+                onTap: () {},
+                child: Text(
+                  'CONTACT',
+                  style: TextStyle(color: Colors.white, fontSize: 22),
+                ),
+              ),
+              Expanded(
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Text(
+                    'Copyright © 2020 | HARSHVARDHAN',
+                    style: TextStyle(
+                      color: Colors.blueGrey[300],
+                      fontSize: 14,
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
